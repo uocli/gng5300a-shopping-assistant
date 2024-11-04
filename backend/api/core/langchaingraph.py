f2f24be43cfd5b08b305d79df2581e3e2219531c
Sean Li <cli049@uottawa.ca>
2024-11-04 14:15:39 -0500

    Initial interaction with customer id

--- backend/api/core/langchaingraph.py
+++ backend/api/core/langchaingraph.py
@@ -5,8 +5,9 @@ from langgraph.constants import START, END
 from langgraph.graph import StateGraph
 from langgraph.prebuilt import tools_condition
 
-from backend.api.utilities import create_tool_node_with_fallback
-from state import (
+from ..services.customers import user_info
+from ..utilities import create_tool_node_with_fallback
+from .state import (
     sensitive_tool_names,
     State,
     Assistant,
@@ -20,14 +21,14 @@ builder = StateGraph(State)
 
 # NEW: The fetch_user_info node runs first, meaning our assistant can see the user's flight information without
 # having to take an action
-# builder.add_node("fetch_user_info", user_info)
-# builder.add_edge(START, "fetch_user_info")
+builder.add_node("fetch_user_info", user_info)
+builder.add_edge(START, "fetch_user_info")
 builder.add_node("assistant", Assistant(assistant_runnable))
-builder.add_edge(START, "assistant")
+# builder.add_edge(START, "assistant")
 builder.add_node("safe_tools", create_tool_node_with_fallback(safe_tools))
 builder.add_node("sensitive_tools", create_tool_node_with_fallback(sensitive_tools))
 # Define logic
-# builder.add_edge("fetch_user_info", "assistant")
+builder.add_edge("fetch_user_info", "assistant")
 
 
 def route_tools(state: State):
@@ -51,7 +52,7 @@ builder.add_edge("safe_tools", "assistant")
 builder.add_edge("sensitive_tools", "assistant")
 
 memory = MemorySaver()
-part_3_graph = builder.compile(
+graph = builder.compile(
     checkpointer=memory,
     # NEW: The graph will always halt before executing the "tools" node.
     # The user can approve or reject (or even alter the request) before
@@ -59,17 +60,16 @@ part_3_graph = builder.compile(
     interrupt_before=["sensitive_tools"],
 )
 
-# # from IPython.display import Image, display
-# from PIL import Image
-# import io
-#
-# try:
-#     # display(Image(part_3_graph.get_graph().draw_mermaid_png()))
-#     # part_3_graph.get_graph().draw_mermaid_png()
-#     image = Image.open(io.BytesIO(part_3_graph.get_graph().draw_mermaid_png()))
-#     image.save("output_image.png")
-#     print("---------1")
-# except Exception:
-#     print("---------")
-#     # This requires some extra dependencies and is optional
-#     pass
+
+def print_graph():
+    from PIL import Image
+    import io
+
+    try:
+        image = Image.open(io.BytesIO(graph.get_graph().draw_mermaid_png()))
+        image.save("graph.png")
+    except:
+        pass
+
+
+# print_graph()
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,78 @@
+import json
+
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from langchain_core.messages import ToolMessage
+
+from .core.langchaingraph import graph
+from .services.customers import get_user_info
+from .utilities import _print_event
 
 
 @csrf_exempt
 def query_view(request):
+    data = json.loads(request.body.decode("utf-8"))
+    customer_id = data.get("customerID", 0)
+    query = data.get("query", "")
+    user_info = get_user_info(customer_id)
+    thread_id = user_info["thread_id"]
+    if thread_id is None:
+        return JsonResponse({"message": "User not found"}, status=404)
 
-    return JsonResponse({"message": "Hello, world!"})
+    config = {
+        "configurable": {
+            # The customer_id is used in our order and cart tools to
+            # fetch the user's order and cart information
+            "customer_id": customer_id,
+            # Checkpoints are accessed by thread_id
+            "thread_id": thread_id,
+        }
+    }
+
+    tutorial_questions = [
+        "How much available balance do I have?",
+        "Do I have any pending orders?",
+        "What are my current orders?",
+        "What's in my cart?",
+    ]
+
+    _printed = set()
+    # We can reuse the tutorial questions from part 1 to see how it does.
+
+    events = graph.stream({"messages": ("user", query)}, config, stream_mode="values")
+    for event in events:
+        _print_event(event, _printed)
+    snapshot = graph.get_state(config)
+    while snapshot.next:
+        # We have an interrupt! The agent is trying to use a tool, and the user can approve or deny it
+        # Note: This code is all outside of your graph. Typically, you would stream the output to a UI.
+        # Then, you would have the frontend trigger a new run via an API call when the user has provided input.
+        try:
+            user_input = input(
+                "Do you approve of the above actions? Type 'y' to continue;"
+                " otherwise, explain your requested changed.\n\n"
+            )
+        except:
+            user_input = "y"
+        if user_input.strip() == "y":
+            # Just continue
+            result = graph.invoke(
+                None,
+                config,
+            )
+        else:
+            # Satisfy the tool invocation by
+            # providing instructions on the requested changes / change of mind
+            result = graph.invoke(
+                {
+                    "messages": [
+                        ToolMessage(
+                            tool_call_id=event["messages"][-1].tool_calls[0]["id"],
+                            content=f"API call denied by user. Reasoning: '{user_input}'. Continue assisting, accounting for the user's input.",
+                        )
+                    ]
+                },
+                config,
+            )
+        snapshot = graph.get_state(config)
+    return JsonResponse({"message": "Success"}, status=200)
