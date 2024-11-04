83e9bc3a0ec824db6e786903e25d2c4282550ce8
Sean Li <cli049@uottawa.ca>
2024-11-03 22:15:40 -0500

    Updated models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, null=False)
+    sku = models.CharField(max_length=255, unique=True, null=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    category = models.CharField(max_length=255, null=False)
     description = models.TextField()
-    sku = models.CharField(max_length=255, unique=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
