ec1a349b1a9cb5d7b89db258fcb33d68427dab10
Sean Li <cli049@uottawa.ca>
2024-11-04 07:44:32 -0500

    Updated models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,6 +7,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, null=False)
     category = models.CharField(max_length=255, null=False)
     description = models.TextField()
+    specification = models.TextField()
 
     def __str__(self):
         return self.name
@@ -14,18 +15,21 @@ class Product(models.Model):
 
 # Draft, Ordered, Shipped, Delivered, Cancelled
 class OrderStatus(models.TextChoices):
-    DRAFT = 'D', "draft"
-    ORDERED = 'O', "ordered"
-    SHIPPED = 'S', "shipped"
-    DELIVERED = 'E', "delivered"
-    CANCELLED = 'C', "cancelled"
+    DRAFT = "D", "draft"
+    ORDERED = "O", "ordered"
+    SHIPPED = "S", "shipped"
+    DELIVERED = "E", "delivered"
+    CANCELLED = "C", "cancelled"
 
 
 class Order(models.Model):
+    customer_id = models.CharField(max_length=255)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=1, choices=OrderStatus.choices, default=OrderStatus.DRAFT)
+    status = models.CharField(
+        max_length=1, choices=OrderStatus.choices, default=OrderStatus.DRAFT
+    )
 
     def __str__(self):
         return f"{self.quantity} of {self.product.name}"
