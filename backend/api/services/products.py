4988aa2377b88b2194091f2e91a75055179e1fe9
Sean Li <cli049@uottawa.ca>
2024-11-04 14:14:22 -0500

    Tools need docstring

--- backend/api/services/products.py
+++ backend/api/services/products.py
@@ -1,21 +1,33 @@
 from langchain_core.tools import tool
 
 from ..models import Product
+from ..serializers import ProductSerializer
 
 
 @tool
-def get_product(product_id: int):
+def get_product(product_id: int) -> dict:
+    """
+    Get product by product ID.
+    :param product_id:
+    :return: a product object
+    """
     products = Product.objects.get(id=product_id)
     if products is None:
-        return None
-    return products[0]
+        return ProductSerializer().data
+    return ProductSerializer(products[0]).data
 
 
 @tool
 def search_products(
     name: str = None, category: str = None, limit: int = 20
 ) -> list[dict]:
-    """Search for products based on name and category."""
+    """
+    Search for products based on name and category.
+    :param name:
+    :param category:
+    :param limit:
+    :return: a list of product objects
+    """
     products = Product.objects.all()
     if name:
         products = products.filter(name__icontains=name)
