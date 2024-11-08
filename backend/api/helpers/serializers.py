991a9b426f87b25b154f214202288f1e8135a959
Sean Li <cli049@uottawa.ca>
2024-11-08 14:26:31 -0500

    Updated docs in serializers.py

--- backend/api/helpers/serializers.py
+++ backend/api/helpers/serializers.py
@@ -4,24 +4,40 @@ from ..models import Product, Order, Customer, OrderItem
 
 
 class CustomerSerializer(serializers.ModelSerializer):
+    """
+    Customer Serializer
+    """
+
     class Meta:
         model = Customer
         fields = "__all__"
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    """
+    Product Serializer
+    """
+
     class Meta:
         model = Product
         fields = "__all__"
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
+    """
+    Order Item Serializer
+    """
+
     class Meta:
         model = OrderItem
         fields = "__all__"
 
 
 class OrderWithItemsSerializer(serializers.ModelSerializer):
+    """
+    Order Serializer with Order Items
+    """
+
     items = OrderItemSerializer(many=True, read_only=True)
 
     class Meta:
