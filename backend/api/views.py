9602d90d051336b7983c75bedefa5183b0664feb
Sean Li <cli049@uottawa.ca>
2024-11-08 14:19:55 -0500

    Updated docs in views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -11,13 +11,21 @@ from .helpers.utilities import print_event
 
 @csrf_exempt
 def query_view(request):
+    """
+    This view is used to handle the user's query.
+    :param request: the query from the user
+    :return: the response to the bot
+    """
     data = json.loads(request.body.decode("utf-8"))
     customer_id = data.get("customerID", 0)
-    thread_id = data.get("threadID", 0)
-    interrupted = data.get("interrupted", False)
+    interrupted = data.get(
+        "interrupted", False
+    )  # This is a indicator to check if the process has been interrupted by sensitive tools
     query = data.get("query", "")
-    user_info = get_user_info(customer_id, thread_id)
+
+    user_info = get_user_info(customer_id)
     thread_id = user_info["thread_id"]
+
     if thread_id is None:
         return JsonResponse({"message": "User not found"}, status=200)
 
@@ -30,24 +38,15 @@ def query_view(request):
             "thread_id": thread_id,
         }
     }
+
     if interrupted:
+        # If the process was interrupted by sensitive tools,
+        # the user will be asked to approve the actions.
         tool_call_id = (
             graph.get_state(config).values["messages"][-1].tool_calls[0]["id"]
         )
         if query.strip() == "y":
-            tool_message = [
-                ToolMessage(
-                    tool_call_id=tool_call_id, content="Client approved to proceed."
-                )
-            ]
-
-            # We now update the state
-            # Notice that we are also specifying `as_node="ask_human"`
-            # This will apply this update as this node,
-            # which will make it so that afterwards it continues as normal
-            # graph.update_state(
-            #     config, {"messages": tool_message}, as_node="sensitive_tools"
-            # )
+            # Approved
             graph.invoke(
                 None,
                 config,
@@ -65,15 +64,7 @@ def query_view(request):
                 config,
             )
 
-        # We can check the state
-        # We can see that the state currently has the `agent` node next
-        # This is based on how we define our graph,
-        # where after the `ask_human` node goes (which we just triggered)
-        # there is an edge to the `agent` node
-        # graph.get_state(config).next
-
     _printed = set()
-    # We can reuse the tutorial questions from part 1 to see how it does.
 
     events = graph.stream({"messages": ("user", query)}, config, stream_mode="values")
     message = ""
@@ -82,45 +73,11 @@ def query_view(request):
         message = print_event(event, _printed)
         state = graph.get_state(config)
         if state.next:
-            print("Interrupted!")
             interrupted = True
             message = (
                 "Do you approve of the above actions? Type 'y' to continue;"
                 " otherwise, explain your requested changed."
             )
-    # snapshot = graph.get_state(config)
-    # while snapshot.next:
-    #     # We have an interrupt! The agent is trying to use a tool, and the user can approve or deny it
-    #     # Note: This code is all outside of your graph. Typically, you would stream the output to a UI.
-    #     # Then, you would have the frontend trigger a new run via an API call when the user has provided input.
-    #     try:
-    #         user_input = input(
-    #             "Do you approve of the above actions? Type 'y' to continue;"
-    #             " otherwise, explain your requested changed.\n\n"
-    #         )
-    #     except:
-    #         user_input = "y"
-    #     if user_input.strip() == "y":
-    #         # Just continue
-    #         result = graph.invoke(
-    #             None,
-    #             config,
-    #         )
-    #     else:
-    #         # Satisfy the tool invocation by
-    #         # providing instructions on the requested changes / change of mind
-    #         result = graph.invoke(
-    #             {
-    #                 "messages": [
-    #                     ToolMessage(
-    #                         tool_call_id=event["messages"][-1].tool_calls[0]["id"],
-    #                         content=f"API call denied by user. Reasoning: '{user_input}'. Continue assisting, accounting for the user's input.",
-    #                     )
-    #                 ]
-    #             },
-    #             config,
-    #         )
-    #     snapshot = graph.get_state(config)
     return JsonResponse(
         {"message": message, "threadID": thread_id, "interrupted": interrupted},
         status=200,
